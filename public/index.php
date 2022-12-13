cfeaa465ba0601e4f9428c4e35eba768d19cb8b4
Akanni Johannes Geoffroy OTEGBEYE <akanni.otegbeye@chem.loc>
2022-12-13 20:14:26 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HiTech | Home</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/normalize.css">
+
+</head>
+<body>
+    <div class="container">
+        <header class="center">
+           <div>
+                <ul>
+                    <li>Options d'affichage / accessibilité</li>
+                    <li>Thème :
+                        <a href="#">sombre</a> 
+                        <a href="#">contrasté</a> 
+                        <a href="#">clair</a> 
+                    </li>
+                    <li>Interlignage
+                        <a href="#">simple</a>
+                        <a href="#">augmenté</a>
+                    </li>
+                </ul>
+           </div>
+           <div class="divheader center" >
+                <a href="index.php">
+                    <img src="./images/logo.svg" alt="Logo">
+                    <h1>HiTech.NET</h1>
+                </a>
+                <h2>Tout le matériel High-Tech</h2>
+                <a href="#">Mon compte</a>
+                <a href="#">Mon panier</a>
+           </div>
+           <nav class="navbar">
+            <ul>
+                <li><a href="">Ordinateurs</a></li>
+                <li><a href="">Périphériques & composants</a></li>
+                <li><a href="">Téléphones</a></li>
+                <li><a href="">Objets connectés</a></li>
+            </ul>
+           </nav>
+
+        </header>
+        <main>
+            <section class="beforpre ">
+                <h3>AOC 24B1XHS</h3>
+                <div class="item flex">
+                    <a href="">Présentation</a>
+                    <a href="">Desciptif</a>
+                    <a href="">Caractéristiques techniques</a>
+                </div>
+            </section>
+
+            <section>
+                <h3>Présentation</h3>
+                <div class="etiquettes">
+                    <p>1920 x 1080 pixels</p>
+                    <p>7 ms</p>
+                    <p>Format large 19/9</p>
+                    <p>Dalle IPS</p>
+                    <p>HDMI</p>
+                    <p>vga</p>
+                    <p>Noir</p>
+                </div>
+
+                <div class="pictures">
+                    <img src="images/product-1/slide-1-lg.jpg" alt="picture-1">
+                    <h4 class="center">Photos non contractuelles</h4>
+                    <div class="mini-picture">
+                        <img src="images/product-1/slide-1-md.jpg" alt="">
+                        <img src="images/product-1/slide-2-md.jpg" alt="">
+                        <img src="images/product-1/slide-3-md.jpg" alt="">
+                        <img src="images/product-1/slide-4-md.jpg" alt="">
+                        <img src="images/product-1/slide-5-md.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="marque">
+                    <h4>Marque: AOC</h4>
+                   <div>
+                        <div>
+                            <div>109€94 <span>dont eco-participation 1€70</span></div>
+                            <button>Ajouter au panier</button>
+                            <button>Ajouter à ma liste d'envie</button>
+                        </div>
+                        <div>Garantie:2 années vendeur
+                        </div>    
+                   </div>
+                   <div>
+                        <p>Un écran 23,8" Full HD et sans bords; c'est sûrement votre futur moniteur pour la maison ou le bureau. AOC vous propose le 24B1XHS, un produit élégant au look doté d'une dalle VA avec un temps de réponse rapide de 7 ms en résolution 1920 x 1080 pixels. Cet écran WLED très fonctionnel offre un contenu avec un fréquence de 60 Hz, idéal pour tous les usages du quotidien.</p>
+                        <ul>
+                            <li>Taille de l'écran:	23.8 pouces</li>
+                            <li> Taille de l'écran:	23.8 pouces</li>
+                            <li> Format de l'écran:	16/9</li>
+                            <li> Type de Dalle:	Dalle IPS</li>
+                            <li> Technologie LCD:	IPS</li>
+                            <li> Résolution Max:	1920 x 1080 pixels</li>
+                            <li> Dot Pitch:	0.2745 mm</li>
+                            <li> Luminosité:	250 cd/m²</li>
+                            <li> Contraste:	20000000 /1</li>
+                            <li> Temps de réponse:	7 ms</li>
+                            <li> Angle de vision (horizontal):	178 Degré(s)</li>
+                            <li> Angle de vision (vertical):	178 Degré(s)</li>
+                            <li> Dalle mate/antireflets:	Oui</li>
+                            <li> NVIDIA GSYNC:	Non</li>
+                            <li> Adaptive-Sync:	Non</li>
+                            <li> AMD FreeSync:	Non</li>
+                            <li> Flicker-Free:	Oui</li>
+                            <li> Anti-lumière bleue:	Oui</li>
+                        </ul>
+                   </div>
+                </div>
+            </section>
+        </main>
+        <footer></footer>
+    </div>
+
+    
+</body>
+</html>
